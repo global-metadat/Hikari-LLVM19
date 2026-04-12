ac2513d42e2607f58b82d3b36e87f092f8463c9d
global-metadat <bah96399@gmail.com>
2026-04-12 10:49:32 +0300

    Add functions for opaque predicates in OpaquePredicates.h
    Added functions to generate opaque predicates and create opaque loads.

--- llvm/include/llvm/Transforms/Obfuscation/OpaquePredicates.h
+++ llvm/include/llvm/Transforms/Obfuscation/OpaquePredicates.h
@@ -1,6 +1,7 @@
 #ifndef _OPAQUEPREDICATES_H_
 #define _OPAQUEPREDICATES_H_
 
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Pass.h"
 
@@ -8,6 +9,16 @@ namespace llvm {
 
 FunctionPass *createOpaquePredicatesPass(bool flag);
 void initializeOpaquePredicatesPass(PassRegistry &Registry);
+
+/// Generate a compound always-true predicate by ANDing `count` random
+/// number-theoretic predicates. `x` must be an i64 Value (typically loaded
+/// from a volatile global). Returns an i1 Value that is provably always true.
+/// Used by OpaquePredicates pass and by BCF for stronger bogus conditionals.
+Value *genCompoundOpaquePredicate(IRBuilder<> &B, Value *x, uint32_t count);
+
+/// Create a volatile-loaded opaque global suitable for use with
+/// genCompoundOpaquePredicate. Returns the LoadInst.
+LoadInst *createOpaqueLoad(IRBuilder<> &B, Module &M, const std::string &name);
 
 } // namespace llvm
 
